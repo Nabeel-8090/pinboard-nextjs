f2811d25747b271214b8f0fafed5b71891f9e3b9
Nabeel-8090 <sheikhnabeel2201182@gmail.com>
2026-04-24 19:42:27 +0500

    fixed a minor issue

--- nextjs-project/app/api/auth/[...nextauth]/route.js
+++ nextjs-project/app/api/auth/[...nextauth]/route.js
@@ -57,11 +57,13 @@ const authOptions = {
 
   secret: process.env.NEXTAUTH_SECRET,
 
-  // 🔥 ADD THIS (IMPORTANT)
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.picture = user.image;
       }
       return token;
     },
@@ -69,6 +71,9 @@ const authOptions = {
     async session({ session, token }) {
       if (token) {
         session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
       }
       return session;
     },
